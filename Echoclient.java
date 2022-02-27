5dbef85f02e4d0241032a606b7dadeafe2771294
Lsobaca <61958964+Lsobaca@users.noreply.github.com>
2022-02-26 22:32:56 -0800

    made the echo server

--- Echoclient.java
+++ Echoclient.java
@@ -5,13 +5,23 @@ import java.io.*;
 public class Echoclient {
     public static void main(String[] args) {
        try {
-           Socket client = new Socket("127.0.0.1",6007);
+            //connects with the echo server on port 6007 using the localhost ip.
+            Socket client = new Socket("127.0.0.1",6007);
+
             PrintWriter pout = new PrintWriter(client.getOutputStream(),true);
             BufferedReader buff = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
             Scanner in = new Scanner(System.in);
+            /*
+            the client is going to prompt the user for some text to yell in a canyon
+            the user would see that the server had responded to the text that the 
+            user has put in.
+            to end the program, the user has to put "end". end can be in any case since
+            we used equalsIngnoreCase.
+            */
             while(true){
                 System.out.println("Enter text: ");
+                System.out.print("Client: ");
                 String echoline = in.nextLine();
                 if ("end".equalsIgnoreCase(echoline)){
                     break;
@@ -19,14 +29,11 @@ public class Echoclient {
                 pout.println(echoline);
                 String reponse = buff.readLine();
                 System.out.println("Server: "+ reponse);
-
-
             }
+
            client.close();
 
-
-
-       } catch (IOException e)
+       } catch (IOException e) {
            //TODO: handle exception
            System.out.println(e);
        } 
